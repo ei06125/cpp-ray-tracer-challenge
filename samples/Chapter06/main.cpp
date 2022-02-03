c4016174f14b1f4bfd592d6a5689aab92a219009
Pedro André Oliveira <ei06125@gcloud.fe.up.pt>
2022-02-03 23:45:24 +0000

    Update samples 06 and 07

--- samples/Chapter06/main.cpp
+++ samples/Chapter06/main.cpp
@@ -1,9 +1,21 @@
 #include "RayTracer.hpp"
 
+using namespace RayTracer::Math;
+
+using namespace RayTracer::Rendering;
+using namespace RayTracer::Rendering::Lighting;
+using namespace RayTracer::Rendering::Materials;
+using namespace RayTracer::Rendering::Primitives;
+using namespace RayTracer::Rendering::Patterns;
+using namespace RayTracer::Rendering::Cameras;
+using namespace RayTracer::Rendering::Scene;
+
+using namespace RayTracer::Profiling;
+
 #ifdef NDEBUG
 constexpr auto g_scale = 10;
 #else
-constexpr auto g_scale = 1;
+constexpr auto g_scale = 5;
 #endif
 
 int main()
@@ -22,13 +34,14 @@ int main()
   auto canvas = Canvas(canvas_pixels, canvas_pixels);
   auto color = make_color(1, 0, 0);
   auto sphere = Sphere();
-  set_transform(sphere, scaling(1 * g_scale, 1 * g_scale, 1 * g_scale));
-  sphere.material.color = make_color(1, 0.2, 1);
+  sphere.SetTransform(scaling(1 * g_scale, 1 * g_scale, 1 * g_scale));
+  sphere.SetMaterial().color = make_color(1, 0.2, 1);
 
   auto light_position = make_point(-10 * g_scale, 10 * g_scale, -10 * g_scale);
   auto light_color = make_color(1, 1, 1);
   auto light = PointLight{ light_position, light_color };
 
+  Timer renderTimer("to render");
   for (auto y = 0; y < canvas_pixels; ++y) {
     // compute the world y coordinate (top = +half, bottom = -half)
     auto world_y = half - pixel_size * y;
@@ -42,12 +55,12 @@ int main()
       auto wall_point = make_point(world_x, world_y, wall_z);
 
       auto r = Ray{ ray_origin, normalize(wall_point - ray_origin) };
-      auto xs = intersect(sphere, r);
+      auto xs = sphere.Intersect(r);
 
-      if (auto aHit = hit(xs); aHit != nullptr) {
-        auto& material = aHit->object.material;
+      if (auto aHit = xs.Hit(); aHit != nullptr) {
+        auto material = aHit->object->GetMaterial();
         auto point = position(r, aHit->t);
-        auto normalv = local_normal_at(aHit->object, point);
+        auto normalv = aHit->object->GetNormalAt(point);
         auto eyev = -(r.direction); // negated
         color = lighting(material, light, point, eyev, normalv);
 
@@ -55,12 +68,26 @@ int main()
       }
     }
   }
+  renderTimer.status();
 
-#ifdef NDEBUG
-  PPMFile ppmFile("image.ppm");
-#else
-  PPMFile ppmFile("imaged.ppm");
+  /// =========================================================================
+  /// @section Saving
+  /// =========================================================================
+
+  std::string filename = "image";
+#ifndef NDEBUG
+  filename += "d";
 #endif
+  filename += "_";
+  const std::chrono::time_point<std::chrono::system_clock> now =
+    std::chrono::system_clock::now();
+  const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+  filename += std::to_string(std::localtime(&t_c)->tm_hour) + "_";
+  filename += std::to_string(std::localtime(&t_c)->tm_min) + "_";
+  filename += std::to_string(std::localtime(&t_c)->tm_sec) + "_";
+  filename += ".ppm";
+
+  PPMFile ppmFile(filename.c_str());
   auto ppm = canvas_to_ppm(canvas);
   ppmFile.Write(ppm.c_str());
 
--- samples/Chapter07/main.cpp
+++ samples/Chapter07/main.cpp
@@ -1,9 +1,21 @@
 #include "RayTracer.hpp"
 
+using namespace RayTracer::Math;
+
+using namespace RayTracer::Rendering;
+using namespace RayTracer::Rendering::Lighting;
+using namespace RayTracer::Rendering::Materials;
+using namespace RayTracer::Rendering::Primitives;
+using namespace RayTracer::Rendering::Patterns;
+using namespace RayTracer::Rendering::Cameras;
+using namespace RayTracer::Rendering::Scene;
+
+using namespace RayTracer::Profiling;
+
 #ifdef NDEBUG
 constexpr auto g_scale = 10;
 #else
-constexpr auto g_scale = 1;
+constexpr auto g_scale = 2;
 #endif
 
 int main()
@@ -12,74 +24,93 @@ int main()
   World world;
 
   // the floor
-  auto floor = Sphere();
-  floor.transform = scaling(10, 0.01, 10);
-  floor.material = Material();
-  floor.material.color = make_color(1, 0.9, 0.9);
-  floor.material.specular = 0;
+  auto floor = std::make_shared<Sphere>();
+  floor->SetTransform(scaling(10, 0.01, 10));
+  floor->SetMaterial().color = make_color(1, 0.9, 0.9);
+  floor->SetMaterial().specular = 0;
   world.AddObject(floor);
 
   // left wall
-  auto left_wall = Sphere();
-  left_wall.transform = translation(0, 0, 5) * rotation_y(-PI / 4) *
-                        rotation_x(PI / 2) * scaling(10, 0.01, 10);
-  left_wall.material = floor.material;
+  auto left_wall = std::make_shared<Sphere>();
+  left_wall->SetTransform(translation(0, 0, 5) * rotation_y(-PI / 4) *
+                          rotation_x(PI / 2) * scaling(10, 0.01, 10));
+  left_wall->SetMaterial(floor->GetMaterial());
   world.AddObject(left_wall);
 
   // right wall
-  auto right_wall = Sphere();
-  right_wall.transform = translation(0, 0, 5) * rotation_y(PI / 4) *
-                         rotation_x(PI / 2) * scaling(10, 0.01, 10);
+  auto right_wall = std::make_shared<Sphere>();
+  right_wall->SetTransform(translation(0, 0, 5) * rotation_y(PI / 4) *
+                           rotation_x(PI / 2) * scaling(10, 0.01, 10));
   world.AddObject(right_wall);
 
   // large middle sphere
-  auto middle = Sphere();
-  middle.transform = translation(-0.5, 1, 0.5);
-  middle.material = Material();
-  middle.material.color = make_color(0.1, 1, 0.5);
-  middle.material.diffuse = 0.7;
-  middle.material.specular = 0.3;
-  world.AddObject(middle);
+  auto middleSphere = std::make_shared<Sphere>();
+  middleSphere->SetTransform(translation(-0.5, 1, 0.5));
+  middleSphere->SetMaterial().color = make_color(0.1, 1, 0.5);
+  middleSphere->SetMaterial().diffuse = 0.7;
+  middleSphere->SetMaterial().specular = 0.3;
+  world.AddObject(middleSphere);
 
   // small right sphere
-  auto right = Sphere();
-  right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5);
-  right.material = Material();
-  right.material.color = make_color(0.5, 1, 0.1);
-  right.material.diffuse = 0.7;
-  right.material.specular = 0.3;
-  world.AddObject(right);
+  auto rightSphere = std::make_shared<Sphere>();
+  rightSphere->SetTransform(translation(1.5, 0.5, -0.5) *
+                            scaling(0.5, 0.5, 0.5));
+  rightSphere->SetMaterial().color = make_color(0.5, 1, 0.1);
+  rightSphere->SetMaterial().diffuse = 0.7;
+  rightSphere->SetMaterial().specular = 0.3;
+  world.AddObject(rightSphere);
 
   // smallest left sphere
-  auto left = Sphere();
-  left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33);
-  left.material = Material();
-  left.material.color = make_color(1, 0.8, 0.1);
-  left.material.diffuse = 0.7;
-  left.material.specular = 0.3;
-  world.AddObject(left);
+  auto leftSphere = std::make_shared<Sphere>();
+  leftSphere->SetTransform(translation(-1.5, 0.33, -0.75) *
+                           scaling(0.33, 0.33, 0.33));
+  leftSphere->SetMaterial().color = make_color(1, 0.8, 0.1);
+  leftSphere->SetMaterial().diffuse = 0.7;
+  leftSphere->SetMaterial().specular = 0.3;
+  world.AddObject(leftSphere);
 
   // light
   world.SetLight(PointLight{ make_point(-10, 10, -10), make_color(1, 1, 1) });
 
   // camera
 #ifdef NDEBUG
-  auto camera = Camera{ 1000, 500, PI / 3 };
+  // High-Definition 16:9
+  auto camera = Camera{ 1280, 720, PI / 3 };
 #else
-  auto camera = Camera{ 100, 50, PI / 3 };
+  // Low-Definition 16:9
+  auto camera = Camera{ 426, 240, PI / 3 };
 #endif
 
   camera.transform = view_transform(
     make_point(0, 1.5, -5), make_point(0, 1, 0), make_vector(0, 1, 0));
 
-  // render the result to a canvas.
-  auto canvas = render(camera, world);
+  /// =========================================================================
+  /// @section Rendering
+  /// =========================================================================
 
-#ifdef NDEBUG
-  PPMFile ppmFile("image.ppm");
-#else
-  PPMFile ppmFile("imaged.ppm");
+  // render the result to a canvas.
+  Timer renderTimer("to render");
+  auto canvas = render(camera, world);
+  renderTimer.status();
+
+  /// =========================================================================
+  /// @section Saving
+  /// =========================================================================
+
+  std::string filename = "image";
+#ifndef NDEBUG
+  filename += "d";
 #endif
+  filename += "_";
+  const std::chrono::time_point<std::chrono::system_clock> now =
+    std::chrono::system_clock::now();
+  const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+  filename += std::to_string(std::localtime(&t_c)->tm_hour) + "_";
+  filename += std::to_string(std::localtime(&t_c)->tm_min) + "_";
+  filename += std::to_string(std::localtime(&t_c)->tm_sec) + "_";
+  filename += ".ppm";
+
+  PPMFile ppmFile(filename.c_str());
   auto ppm = canvas_to_ppm(canvas);
   ppmFile.Write(ppm.c_str());
 
